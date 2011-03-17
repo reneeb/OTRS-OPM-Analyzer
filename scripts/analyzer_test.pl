c7baaf818ce723dd2bdb199ada92d118a88ef396
reneeb <github@renee-baecker.de>
2011-03-17 15:57:22 +0100

    add script for local analysis

---
+++ scripts/analyzer_test.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use File::Basename;
+use File::Spec;
+use File::Temp;
+use FindBin;
+
+use lib $FindBin::Bin . '/../lib';
+use OTRS::OPM::Analyzer;
+
+use Data::Dumper;
+
+my $file     = $ARGV[0];
+
+if ( !$file || !-f $file ) {
+    print "$0 <opm_file>";
+    exit;
+}
+
+my $config   = $FindBin::Bin . '/../conf/base.yml';
+my $analyzer = OTRS::OPM::Analyzer->new(
+    configfile => $config,
+    roles => {
+        opm => [qw/Dependencies/],
+    },
+);
+my $results  = $analyzer->analyze( $file );
+
+print Dumper $results;
