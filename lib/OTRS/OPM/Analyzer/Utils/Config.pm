6249d307a76635a6479d764e09c2360fa3e4ad76
reneeb <github@renee-baecker.de>
2014-01-17 10:02:20 +0100

    add set method and fix some bugs

--- lib/OTRS/OPM/Analyzer/Utils/Config.pm
+++ lib/OTRS/OPM/Analyzer/Utils/Config.pm
@@ -18,7 +18,7 @@ sub new{
     my $default_file = File::Spec->catfile( $default_dir,  'conf', 'base.yml' );
        $default_file = File::Spec->rel2abs( $default_file );
     
-    $file ||= $default_file;
+    $file ||= $default_file if -e $default_file;
     $self->load( $file ) if defined $file;
     return $self;
 }
@@ -36,11 +36,14 @@ sub get {
     my $return;
 
     if( defined $key ){
-        my $config = $self->{_config};
+        my $config = $self->{_config} || {};
 
         my @keys = split /(?<!\\)\./, $key;
         for my $subkey ( @keys ){
             $subkey =~ s/\\\././g;
+
+            return if $config and $subkey and ref $config ne 'HASH';
+
             return if not exists $config->{$subkey};
             $config = $config->{$subkey};
         }
@@ -51,4 +54,31 @@ sub get {
     $return;
 }
 
+sub set {
+    my ($self,$key,$value) = @_;
+
+    $self->{_config} ||= {};
+
+    if( defined $key ){
+        my $config = $self->{_config} || {};
+
+        my @keys = split /(?<!\\)\./, $key;
+        while ( my $subkey = shift @keys ){
+            $subkey =~ s/\\\././g;
+
+            last if $subkey and $config and ref $config ne 'HASH';
+
+            $config->{$subkey} = {} if not exists $config->{$subkey};
+
+            if ( !@keys ) {
+                $config->{$subkey} = $value;
+            }
+            else {
+                $config = $config->{$subkey};
+            }
+        }
+    }
+}
+
 1;
+
