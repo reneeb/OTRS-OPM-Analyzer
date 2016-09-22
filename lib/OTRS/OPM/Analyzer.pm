2b83a6c07986f229e31170a4ed966a59011391ea
reneeb <info@perl-services.de>
2016-09-22 11:55:58 +0200

    bump version

--- lib/OTRS/OPM/Analyzer.pm
+++ lib/OTRS/OPM/Analyzer.pm
@@ -8,7 +8,7 @@ use Moose::Util::TypeConstraints;
 use OTRS::OPM::Analyzer::Utils::OPMFile;
 use OTRS::OPM::Analyzer::Utils::Config;
 
-our $VERSION = 0.04;
+our $VERSION = 0.05;
 
 # define types
 subtype 'OPMFile' =>
